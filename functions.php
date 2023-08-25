3b0d8d1f46115ce61420cc9e540c8f1d6f93d418
Samson Zhang <wwsamson@yahoo.com>
2023-08-25 02:16:50 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function remove_admin_login_header()
+{
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+
+add_action('get_header', 'remove_admin_login_header');
+
+function script_enqueue()
+{
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/style.css', false, NULL, 'all' );
+    wp_enqueue_style('twstyle', get_template_directory_uri() . '/css/tw.css', false, NULL, 'all' );
+    wp_enqueue_style('fontawesome', "https://use.fontawesome.com/releases/v6.4.0/css/all.css", false, NULL, 'all' );
+}
+add_action('wp_enqueue_scripts', 'script_enqueue');
+
+add_theme_support( 'post-thumbnails' );
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./**/*.php"
+    ],
+    theme: {
+      extend: {
+        "colors": {
+            "tblue": "#38557D",
+            "tlightblue": "#A5BFD3",
+            "tyellow": "#EDC065",
+            "tlightyellow": "#F4D596",
+            "tred": "#AC3437",
+            "tgray": "#343534",
+            "tdark": "#111111",
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
