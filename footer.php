365fa1bc66dd7697f161af1fed3335507e7c2d39
Samson Zhang <wwsamson@yahoo.com>
2023-08-26 22:36:30 -0400

    wip footer

--- footer.php
+++ footer.php
@@ -1,2 +1,30 @@
-<div>footer</div>
+    <div class="w-full bg-tblue text-white border-b-8 border-tyellow">
+        <div class="max-w-4xl mx-auto px-4">
+            <img src="<?php echo get_template_directory_uri() . "/img/logo-white.svg";?>" alt="" class="w-32 mx-auto block py-12"/>
+            <div class="flex gap-8">
+                <div class="w-1/2 text-lg">
+                    <p>JCal is a free program that immerses California high school students into the state’s news ecosystem. It is a collaboration between the <a href="https://aaja.org" class="underline">Asian American Journalists Association</a> and <a href="https://calmatters.org" class="underline">CalMatters</a>.</p>
+                </div>
+                <div class="w-1/2 text-lg flex flex-col items-end">
+                    <p>Contact us at <a href="mailto:michael@calmatters.org" class="underline">michael@calmatters.org</a> or <a href="mailto:support@aaja.org" class="underline">support@aaja.org</a>.</p>
+                    <a class="py-1 px-2 bg-tyellow font-black text-tdark uppercase mt-auto inline-block" href="">Subscribe to aaja’s newsletter</a>
+                </div>
+            </div>
+            <div class="flex items-center my-6 gap-4">
+                <img src="<?php echo get_template_directory_uri() . "/img/calmatters-badge.png" ?>" class="h-16" alt=""/>
+                <a href="https://calmatters.org">calmatters.org</a>
+                <a href=""><i class="fa-brands fa-twitter"></i>t</a>
+                <a href=""><i class="fa-brands fa-instagram"></i>i</a>
+                <a href="https://calmatters.org" class="ml-auto">aaja.org</a>
+                <a href=""><i class="fa-brands fa-twitter"></i>t</a>
+                <a href=""><i class="fa-brands fa-instagram"></i>i</a>
+                <img src="<?php echo get_template_directory_uri() . "/img/aaja-badge.png"?>" class="h-16" alt=""/>
+            </div>
+            <div class="flex items-center font-bold gap-8 py-5 border-t border-[#6187AD] mt-5">
+                <a href="">Stories</a>
+                <a href="">People</a>
+                <a href="">About</a>
+            </div>
+        </div>
+    </div>
 </body>
