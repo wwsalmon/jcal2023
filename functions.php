5cb8e54abd087de4bfb80045c8129af9f77a1e58
Samson Zhang <wwsamson@yahoo.com>
2023-08-27 22:19:01 -0700

    wip people page

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ add_action('get_header', 'remove_admin_login_header');
 
 function script_enqueue()
 {
-    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/style.css', false, NULL, 'all' );
     wp_enqueue_style('twstyle', get_template_directory_uri() . '/css/tw.css', false, NULL, 'all' );
     wp_enqueue_style('fontawesome', "https://use.fontawesome.com/releases/v6.4.0/css/all.css", false, NULL, 'all' );
--- page-people.php
+++ page-people.php
@@ -1,2 +1,10 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<?php get_header(); while (have_posts()) : the_post(); ?>
+    <div class="max-w-4xl mx-auto px-4">
+        <h1 class="text-4xl sm:text-5xl text-tblue uppercase font-black pt-24">People</h1>
+        <p class="text-xl sm:text-3xl text-tgray mt-8 !leading-snug"><?php echo get_the_excerpt(); ?></p>
+        <?php echo get_the_post_thumbnail( null, "full", array("class" => "w-full block mt-8")); ?>
+        <div class="flex">
+            <p class="text-tgray text-right mt-8 ml-auto w-64"><?php echo get_the_post_thumbnail_caption() ?></p>
+        </div>
+    </div>
+<?php endwhile; get_footer(); ?>
