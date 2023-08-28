fd2c07f89f1ab4af5904e49c7a15e87fb78f67bf
Samson Zhang <wwsamson@yahoo.com>
2023-08-27 20:26:41 -0700

    add links to header

--- header.php
+++ header.php
@@ -13,15 +13,15 @@ wp_head();
 <body>
     <div class="h-20 w-full sticky top-0 bg-tdark text-white z-10">
         <div class="flex items-center w-full h-full border-t-8 box-border border-tyellow px-4">
-            <p>A partnership between <a href="" class="text-tyellow">AAJA</a> and <a href="" class="text-tyellow">CalMatters</a></p>
+            <p>A partnership between <a href="http://aaja.org/" class="text-tyellow">AAJA</a> and <a href="https://calmatters.org/youthjournalism/" class="text-tyellow">CalMatters</a></p>
             <div class="ml-auto flex items-center gap-6">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <a href="" class="font-semibold">Stories</a>
-                <a href="" class="font-semibold">People</a>
-                <a href="" class="font-black uppercase px-2 py-1 bg-tyellow text-tdark">About JCal</a>
+                <a href="<?php echo home_url("/search"); ?>"><i class="fa-solid fa-magnifying-glass"></i></a>
+                <a href="<?php echo home_url("/"); ?>" class="font-semibold">Stories</a>
+                <a href="<?php echo home_url("/people"); ?>" class="font-semibold">People</a>
+                <a href="<?php echo home_url("/about"); ?>" class="font-black uppercase px-2 py-1 bg-tyellow text-tdark">About JCal</a>
             </div>
         </div>
-        <div class="absolute left-1/2 top-0 bg-white h-20 w-40 rounded-b-full text-tdark transform -translate-x-1/2 flex items-center justify-center">
+        <a class="absolute left-1/2 top-0 bg-white h-20 w-40 rounded-b-full text-tdark transform -translate-x-1/2 flex items-center justify-center" href="<?php echo get_home_url("/"); ?>">
             <img src="<?php echo get_template_directory_uri() . "/img/logo.svg" ?>" alt="" class="w-[92px] -mt-3">
-        </div>
+        </a>
     </div>
