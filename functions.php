ac6a5590256f789f708abd2eca041eb74b9622a8
Samson Zhang <wwsamson@yahoo.com>
2024-09-11 01:30:26 -0700

    add editor styles

--- functions.php
+++ functions.php
@@ -40,6 +40,7 @@ add_action("init", "add_jcal_blocks");
 
 add_action("enqueue_block_editor_assets", function() {
     wp_enqueue_style('twstyle', get_template_directory_uri() . '/css/tw.css', false, NULL, 'all' );
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 });
 
 add_filter("excerpt_length", function() {return 20;}, 999);
@@ -91,3 +92,6 @@ function jcal_customizer_setup($wp_customize) {
 }
 
 add_action('customize_register', 'jcal_customizer_setup');
+
+add_theme_support('editor-styles');
+add_editor_style('./css/editor.css');
