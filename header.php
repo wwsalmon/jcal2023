48b63bf7cefc41c5c6867761f3b3881d08444801
Samson Zhang <wwsamson@yahoo.com>
2024-09-15 19:10:27 -0700

    add categories to desktop navbar

--- header.php
+++ header.php
@@ -19,7 +19,29 @@ $is_home = ($template_name === "index.php");
             <p class="hidden sm:block"><span class="hidden lg:inline">A partnership between </span><a href="http://aaja.org/" class="<?php if ($is_home) echo "text-tyellow"; else echo "text-tblue" ?>">AAJA</a><span class="hidden lg:inline"> and </span><span class="inline lg:hidden"> / </span><a href="https://calmatters.org/youthjournalism/" class="<?php if ($is_home) echo "text-tyellow"; else echo "text-tblue" ?>">CalMatters</a></p>
             <div class="ml-auto md:flex items-center gap-6 hidden">
                 <a href="<?php echo home_url("/?s="); ?>"><i class="fa-solid fa-magnifying-glass"></i></a>
-                <a href="<?php echo home_url("/"); ?>" class="font-semibold">Stories</a>
+                <div class="font-semibold relative" id="dropdown">
+                    <span>Stories <i class="fa-solid fa-caret-down ml-1"></i></span>
+                    <div class="hidden absolute right-0 pt-4 text-black" id="dropdown-menu">
+                        <?php
+                        $categories = get_categories();
+                        foreach($categories as $category): if ($category->name !== "Uncategorized"):
+                        
+                            $cat_name = $category->name;
+                            $cat_explode = explode(":", $cat_name);
+                            if (count($cat_explode) > 1) {
+                                $cat_year = substr($cat_explode[0], 0, 4);
+                                $cat_name = $cat_year . ": " . $cat_explode[1];
+                            }
+                            ?>
+                            <a href="<?php echo get_category_link($category) ?>" class="px-4 py-2 block bg-white hover:bg-gray-100 whitespace-nowrap"><?php echo $cat_name;?></a>
+                        <?php endif; endforeach; ?>
+                    </div>
+                </div>
+                <style>
+                    #dropdown:hover #dropdown-menu {
+                        display: block;
+                    }
+                </style>
                 <a href="<?php echo home_url("/people"); ?>" class="font-semibold">People</a>
                 <a href="<?php echo home_url("/about"); ?>" class="font-black uppercase px-2 py-1 bg-tyellow text-tdark">About<span class="hidden lg:inline"> JCal</span></a>
             </div>
